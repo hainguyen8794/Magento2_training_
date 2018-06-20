ccd870366c892f89964c824cbb278ff0d7b5f521
hainh <hainh080794@gmail.com>
2018-06-20 23:38:33 +0700

    fix require js

--- requirejs/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-requirejs.config({ // cau hinh` main cua? require.js
-    paths: {
-        'requirejs': 'lib/knockout', // thu vien cua? knockout
-        'demo' : 'knockout' // hello world
-    }
-});
-
-requirejs(['demo']);
